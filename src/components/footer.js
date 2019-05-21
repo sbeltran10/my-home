990a90a3cff476d25b6dc5f9ccd5d68634340b27
sbeltran10 <s.beltran10@uniandes.edu.co>
2019-05-20 17:35:50 -0700

    typo and adjusted mobile padding

--- src/components/footer.js
+++ src/components/footer.js
@@ -44,7 +44,7 @@ const Footer = () => (
         </div>
         <div className="footer-text">Icons made by <a href="https://www.flaticon.com/authors/mavadee" title="mavadee">mavadee</a>, <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> and <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> are licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a></div>
         <div className="footer-text">
-          Copyright © santibelca.com 2019
+          Copyright © santibeltran.com 2019
         </div>
       </footer>
     }
