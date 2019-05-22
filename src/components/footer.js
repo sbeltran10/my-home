0955369c62668dad7a2cc395a80e32925e74478e
sbeltran10 <s.beltran10@uniandes.edu.co>
2019-05-22 11:58:24 -0700

    small hover style modifications

--- src/components/footer.js
+++ src/components/footer.js
@@ -42,8 +42,11 @@ const Footer = () => (
             santibelca@gmail.com
         </a>
         </div>
-        <div className="footer-text">Icons made by <a href="https://www.flaticon.com/authors/mavadee" title="mavadee">mavadee</a>, <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> and <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> are licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a></div>
         <div className="footer-text">
+          Site built with <a href="https://www.gatsbyjs.org/" target="_blank" rel="noopener noreferrer">GatsbyJS</a>
+        </div>
+        <div className="footer-text">Icons made by <a href="https://www.flaticon.com/authors/mavadee" title="mavadee">mavadee</a>, <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> and <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> are licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a></div>
+        <div className="footer-text"> 
           Copyright © santibeltran.com 2019
         </div>
       </footer>
