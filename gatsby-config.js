b6bf4df373662fe60bd52528200775d4353cd936
sbeltran10 <s.beltran10@uniandes.edu.co>
2019-05-22 21:41:48 -0700

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,14 @@ module.exports = {
       },
     },
     `gatsby-plugin-sass`,
-    `gatsby-plugin-sitemap`
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-140721055-1",        
+        head: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
